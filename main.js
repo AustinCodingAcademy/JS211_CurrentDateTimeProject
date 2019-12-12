db4ed0546d4f85662abcd081c5a2bbbb59971c24
HippErger <berger.clayton@gmail.com>
2019-12-12 13:26:21 -0600

    added comments to the main.js file

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-// Write a JavaScript program to display the current day and time.
-
+// ***************************
+//          PART ONE
+// ***************************
+// Write a JavaScript program to display the current day and time, start with:
+console.log(new Date())
 
 
 // Write a JavaScript program to convert a number to a string.
@@ -34,4 +37,17 @@
 
 // Write a JavaScript program that runs when both things are not true.  
 
+// ***************************
+//         PART TWO
+// ***************************
 
+// 1. go to `index.html` 
+// 2. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
+// **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTION!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
+
+
+
+// Additional Resources
+// Video1: https://player.vimeo.com/video/377147232
+// Video2: https://www.youtube.com/embed/bkvH28PXLWc
+// Video3: https://www.youtube.com/embed/TrGI9Yki-24
