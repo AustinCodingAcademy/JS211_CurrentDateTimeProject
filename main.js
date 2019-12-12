1ee16d78a152443e6d306ce19f36a8788a50edd7
HippErger <berger.clayton@gmail.com>
2019-12-12 13:18:52 -0600

    added instructions for the project

---
+++ main.js
@@ -0,0 +1,37 @@
+// Write a JavaScript program to display the current day and time.
+
+
+
+// Write a JavaScript program to convert a number to a string.
+
+
+
+// Write a JavaScript program to convert a string to the number.
+
+
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+  // * Boolean
+  // * Null
+  // * Undefined
+  // * Number
+  // * NaN
+  // * String
+  
+
+  
+// Write a JavaScript program that adds 2 numbers together.
+
+
+
+// Write a JavaScript program that runs only when 2 things are true.
+
+
+
+// Write a JavaScript program that runs when 1 of 2 things are true.
+
+
+
+// Write a JavaScript program that runs when both things are not true.  
+
+
