a94fb5b223aaececd3078e1a1ab0c755daec23ee
TEldridge <skippydrumskins@yahoo.com>
2020-07-17 12:51:25 -0500

    set working functions and buttons

--- main.js
+++ main.js
@@ -9,20 +9,61 @@ console.log(new Date)
 
 const displayDate = () => {
   const currentDate = new Date()
-
   document.getElementById("display-element").innerHTML = currentDate;
+  document.getElementById("display-type").innerHTML = null
 }
  
 
 // Write a JavaScript program to convert a number to a string.
+
+const storeNumber = (a) => {
+ let inputString = a
+ changeNum(inputString)
+ console.log(inputString)
+}
+
+
+const changeNum = (a) => {
+  let string = a.toString()
+  document.getElementById("display-element").innerHTML = string;
+  document.getElementById("display-type").innerHTML = typeof string;
+  console.log(string)
+  console.log(typeof string)
+}
 
 
 
 // Write a JavaScript program to convert a string to the number.
 
 
+const storeString = (string) => {
+  let inputNum = string
+  changeString(inputNum)
+  console.log(inputNum)
+}
+
+const changeString = (a) => {
+  let string = a;
+  let num = parseInt(string);
+  document.getElementById("display-element").innerHTML = num;
+  document.getElementById("display-type").innerHTML = typeof num;
+  console.log(num)
+  console.log(typeof num)
+}
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+
+// const anyType = () => {
+
+// }
+
+const identifyType = (type) => {
+  console.log(typeof type)
+  document.getElementById("display-element").innerHTML = type;
+  document.getElementById("display-type").innerHTML = typeof type;
+} 
+
+
   // * Boolean
   // * Null
   // * Undefined
@@ -34,17 +75,78 @@ const displayDate = () => {
   
 // Write a JavaScript program that adds 2 numbers together.
 
+const addNumbers = () => {
+	let val1 = parseInt(document.getElementById("value1").value);
+	let val2 = parseInt(document.getElementById("value2").value);
+  document.getElementById("display-element").innerHTML = val1 + val2;
+  document.getElementById("display-type").innerHTML = null
+  }
 
+
+//  let firstNum = null
+//  let secondNum = null
+ 
+// const saveFirstNumber = (num) => {
+//   firstNum = parseInt(num)  
+// }
+
+// const saveSecondNumber = (num) => {
+//   secondNum = parseInt(num)
+// }
+
+// const addNum = (num1, num2) => {
+//   let sum = num1 + num2
+//   document.getElementById("display-element").innerHTML = sum;
+//   document.getElementById("display-type").innerHTML = typeof sum;  
+// }
+
+// const addNum = () => {
+//   document.getElementById("display-element").innerHTML = additionForm(firstNum + secondNum);
+//   document.getElementById("display-type").innerHTML = typeof additionForm(firstNum + secondNum);
 
 // Write a JavaScript program that runs only when 2 things are true.
 
+
+const confirmTruth = () => {
+  let x = parseInt(document.getElementById("valueX").value);
+  let y = parseInt(document.getElementById("valueY").value);
+  if(x && y){
+    document.getElementById("display-element").innerHTML = "True";
+  } else {
+    document.getElementById("display-element").innerHTML = "False";  
+  }
+  
+  // document.getElementById("display-element").innerHTML = ansD = val1 + val2;
+  // document.getElementById("display-type").innerHTML = null
+}
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
 
 
-// Write a JavaScript program that runs when both things are not true.  
+const confirmLie = () => {
+  let x = parseInt(document.getElementById("valueX").value);
+  let y = parseInt(document.getElementById("valueY").value);
+  if(x || y){
+    document.getElementById("display-element").innerHTML = "True";
+  } else {
+    document.getElementById("display-element").innerHTML = "False";  
+  }
+}
+
+
+// // Write a JavaScript program that runs when both things are not true.  
+
+const confirmFalse = () => {
+  let x = parseInt(document.getElementById("valueX").value);
+  let y = parseInt(document.getElementById("valueY").value);
+  if(!x || !y){
+    document.getElementById("display-element").innerHTML = "True";
+  } else {
+    document.getElementById("display-element").innerHTML = "False";  
+  }
+}
 
 // ***************************
 //         PART TWO
