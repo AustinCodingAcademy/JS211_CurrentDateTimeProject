9b99086cd9abb711caa6a8e29860632470369cea
priscillamartinez <priscillam54@gmail.com>
2020-12-02 16:18:33 -0600

    changes to main.js

--- main.js
+++ main.js
@@ -16,35 +16,83 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
-
+let x = 27;
+console.log(x.toString(), x);
 
 // Write a JavaScript program to convert a string to the number.
 
-
+let z = "55";     //the variable z hold the string 55
+parseInt(z);      //the string is converted into a number
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
+  const myBool = true;
   // * Null
+  const myNull = null;
   // * Undefined
+  const myUndef = undefined;
   // * Number
+  const num = 7;
   // * NaN
+  const myNan = NaN;
   // * String
+  const myString = "I am a string"
+
+  const isTypeOf = (data) => {
+    return console.log(typeof data);
+  }
   
+  // function invocation
+  isTypeOf(myBool);
+  isTypeOf(myNull);
+  isTypeOf(myUndef);
+  isTypeOf(num);
+  isTypeOf(myNan);
+  isTypeOf(myString);
 
   
 // Write a JavaScript program that adds 2 numbers together.
+const addTwoNumbers = (num1, num2) => {
+  return num1 + num2
+}
 
+// addTwoNumbers(7, 54)
 
 
 // Write a JavaScript program that runs only when 2 things are true.
+let a = 2;
+let b = 2;
+let c = 6;
 
+const trueTwo = (a, b, c) => {
+  if ((a === b) && (c > b)){
+    return true;
+  }
+}
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
+let a = 2;
+let b = 2;
+let c = 1;
 
+const trueTwo = (a, b, c) => {
+  if ((a === b) || (c > b)){
+    return true;
+  }
+}
 
+// Write a JavaScript program that runs when both things are not true.
 
-// Write a JavaScript program that runs when both things are not true.  
+let a = 6;
+let b = 5;
+let c = 1;
+
+const trueTwo = (a, b, c) => {
+  if ((a === b) || (c > b)){
+    return false;
+  }
+}
 
 // ***************************
 //         PART TWO
